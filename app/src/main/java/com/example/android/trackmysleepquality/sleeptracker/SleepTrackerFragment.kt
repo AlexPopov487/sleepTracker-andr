1dca26ae4841f7ce500ccf22b00ca5ce53f11bb6
Alex Popov <apopov974@gmail.com>
2021-02-01 21:47:59 +0500

    app working fine with db! Ready to add recyclerView!

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * A fragment with buttons to record start and end times for sleep, which are saved in
+ * a database. Cumulative data is displayed in a simple scrollable TextView.
+ * (Because we have not learned about RecyclerView yet.)
+ */
+class SleepTrackerFragment : Fragment() {
+
+    /**
+     * Called when the Fragment is ready to display content to the screen.
+     *
+     * This function uses DataBindingUtil to inflate R.layout.fragment_sleep_quality.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_sleep_tracker, container, false)
+
+        // If I get it right, this is just a context of the whole application
+        val application = requireNotNull(this.activity).application
+
+        // this is the instance of SleepDAO needed to be passed into the ViewModel factory
+        // along with the application
+        val sleepDataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        // we take the same params in the viewModelFactory as we do in the viewModel (DAO and APPLICATION)
+        // because we need to get access to the database right when the viewModel is created
+        // and not to instantiate the sleepDao in the viewModel after it is created
+        val viewModelFactory = SleepTrackerViewModelFactory(sleepDataSource, application)
+
+        val sleepTrackerViewModel = ViewModelProvider(this, viewModelFactory)
+                .get(SleepTrackerViewModel::class.java)
+
+        // if navigateToSleepQualityFragment variable is empty we will not navigate at all
+        // otherwise, we will perform navigation passing along the night id
+        sleepTrackerViewModel.navigateToSleepQualityFragment.observe(viewLifecycleOwner, Observer {
+            night ->
+            night?.let { this.findNavController().navigate(SleepTrackerFragmentDirections
+                    .actionSleepTrackerFragmentToSleepQualityFragment(night.nightId))
+                sleepTrackerViewModel.doneNavigation()
+            }
+        })
+
+        // show snackBar when data is wiped
+        sleepTrackerViewModel.showStackBarEvent.observe(viewLifecycleOwner, Observer {showSnackBar ->
+            if (showSnackBar) {
+                Snackbar.make(activity!!.findViewById(android.R.id.content),
+                        R.string.cleared_message,
+                        Snackbar.LENGTH_SHORT)
+                        .show()
+                sleepTrackerViewModel.doneShowingSnackBar()
+            }
+        })
+
+        // connect viewModel declaration from XML with the actual viewModel
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+        // this allows us to always get updated live data
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
+
+/**
+ * ViewModel for SleepTrackerFragment.
+ */
+class SleepTrackerViewModel(
+        val database: SleepDatabaseDao,
+        application: Application) : AndroidViewModel(application) {
+
+    private var tonight = MutableLiveData<SleepNight?>()
+
+    private val nights = database.getAllNights()
+
+    // enable the start button IF tonight value is empty
+    val startButtonVisible = Transformations.map(tonight){
+        it == null}
+    // enable the stop button when there is tonight
+    val stopButtonVisible = Transformations.map(tonight) {
+        it != null}
+    // enable the stop button when there is data in database
+    val clearButtonVisible = Transformations.map(nights) {
+        it.isNotEmpty()}
+
+
+    // we need to reformat nights variable so that we can show it in the UI
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    // if this equals to null we will not navigate.
+    private var _navigateToSleepQualityFragment = MutableLiveData<SleepNight>()
+    val navigateToSleepQualityFragment: LiveData<SleepNight>
+        get() = _navigateToSleepQualityFragment
+
+    fun doneNavigation() {
+        _navigateToSleepQualityFragment.value = null
+    }
+
+
+    private var _showSnackBarEvent = MutableLiveData<Boolean>()
+    val showStackBarEvent: LiveData<Boolean>
+        get() = _showSnackBarEvent
+
+    fun doneShowingSnackBar(){
+        _showSnackBarEvent.value = false
+    }
+
+    init {
+        initializeTonight()
+    }
+
+
+    private fun initializeTonight() {
+        viewModelScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+
+        return withContext(Dispatchers.IO) {
+            var currentNight = database.getTonight()
+
+            // when created, startTime and endTime of the currentNight are the same until we press
+            // the stop button updating the endTime value.
+            // That said, if startTime and endTime are not equal at this step, the night was not
+            // created at all and we assign it to null
+            if (currentNight?.startTimeMillis != currentNight?.endTimeMillis) {
+                currentNight = null
+            }
+            currentNight
+        }
+    }
+
+    fun onStartTracking() {
+        viewModelScope.launch {
+            val startTheNight: SleepNight = SleepNight()
+            insertNight(startTheNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insertNight(newNight: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(newNight)
+        }
+    }
+
+    fun onStopTracking() {
+        viewModelScope.launch {
+            val stopTheNight = tonight.value ?: return@launch
+            stopTheNight.endTimeMillis = System.currentTimeMillis()
+            updateNight(stopTheNight)
+            _navigateToSleepQualityFragment.value = stopTheNight
+        }
+    }
+
+    private suspend fun updateNight(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    fun onClear() {
+        viewModelScope.launch {
+            clearNightTable()
+
+            //clear the tonight value as well so the operations like update cannot be completed
+            tonight.value = null
+
+            // show shackBar
+            _showSnackBarEvent.value = true
+        }
+    }
+
+    private suspend fun clearNightTable() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+
+}
+
